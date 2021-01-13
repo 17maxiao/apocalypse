e4b0c8fc4131f9ddde2725a8bed00a0139fdf7bf
May X <17maxiao@gmail.com>
2021-01-13 10:35:58 -0500

    video and media support + art info below pieces

--- gallery/src/components/Gallery.js
+++ gallery/src/components/Gallery.js
@@ -5,21 +5,55 @@ import ScrollMenu from "react-horizontal-scrolling-menu";
 import "../style/Gallery.css";
 
 let list = [
-    {name: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1412623598139-BC6T79X3DJ5IBRG3UMK3/ke17ZwdGBToddI8pDm48kEfj2iehcImfUrNPp_p3mIQUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dvVMhtqkTAeLq_Ad5NjnhUgZRGpRgWfG6TFTdciSykqMbSexTd1-frD7527z4SM9QQ/image-asset.jpeg?format=2500w"},
-    {name: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1606268249679-TXGZ0IL3WVUR2UUIKWO5/ke17ZwdGBToddI8pDm48kMFiMyT1nneRMhnmfuSfpxZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mlM0or4nqX7jrn5yWu0hA1QXedaIFqnAbw_tQShHbKg4-O_KAc44ak5jGzrnn7f3A/image-asset.jpeg?format=1500w"},
-    {name: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1606268226004-Z7B2FOF8KQHRRK3B5LL5/ke17ZwdGBToddI8pDm48kPT1Nkx3ySqTQzCFwVjaPQEUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2doWNX6v4NUZXVvq55gptTczsEhgvyXzBMRDIk3x86TC9JvwGh1qtNWvMhYKnvaKhbA/image-asset.jpeg?format=2500w"},
-    {name: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1606445947263-AGJM7ZIM1RXJR1Z5WWBP/ke17ZwdGBToddI8pDm48kIPMjyAxYAbEcFTs8zCKKJJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0qN_-Z3B7EvygvPOPmeOryX1pkXg-pKyyA7MDP6ZuEHmJuzC89-qxFpXVqq6ylzIAA/***E+White+-The+Spice+Channel-+--NOT+FINAL+VERSION-PLEASE+PHOTO+in+AMS--+2020.jpg?format=2500w"},
-];
+    {type: "video/mp4", name: "Herzog", artist: "ACV (he/him)", year: "2020", 
+    dsc: "Born Alexandre Vachon, ACV grew up in Montreal, Quebec. As both an activist and musician, ACV’s work frequently discusses both systemic racism as well as his own experience as a Black man in Canada. Herzog (and its accompanying music video) is from ACV’s album entitled Saul Bellow Négrophilique, where ACV interacts with Saul Bellow’s novels from the perspective of a Black Montrealer. ACV’s Herzog, named after the corresponding Saul Bellow novel, depicts a narrator who at first listen explains his own shortcomings in a failed relationship and his reactions against his former partner. But the lyrics also reveal something deeper: how Black people and other marginalized individuals grow up in broken environments that discourage emotional vulnerability and self-reflection. With Herzog, ACV sheds light on the mental health struggle that systematic racism inflicts and demonstrates the difficult journey to accepting self-growth and vulnerability that Black individuals face because of this.", 
+    link: "https://www.youtube.com/embed/ySvIoOs9Cr8"},
+    {type: "image", name: "Down in Front: Paramount's", artist: "Eric White", year: "Unknown", dsc: "Description", link: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1412623598139-BC6T79X3DJ5IBRG3UMK3/ke17ZwdGBToddI8pDm48kEfj2iehcImfUrNPp_p3mIQUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dvVMhtqkTAeLq_Ad5NjnhUgZRGpRgWfG6TFTdciSykqMbSexTd1-frD7527z4SM9QQ/image-asset.jpeg?format=2500w"},
+    {type: "image", name: "Claim the Echo", artist: "Eric White", year: "Unknown", dsc: "Description", link: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1606268249679-TXGZ0IL3WVUR2UUIKWO5/ke17ZwdGBToddI8pDm48kMFiMyT1nneRMhnmfuSfpxZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mlM0or4nqX7jrn5yWu0hA1QXedaIFqnAbw_tQShHbKg4-O_KAc44ak5jGzrnn7f3A/image-asset.jpeg?format=1500w"},
+    {type: "image", name: "Late Shift", artist: "Eric White", year: "Unknown", dsc: "Description", link: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1606268226004-Z7B2FOF8KQHRRK3B5LL5/ke17ZwdGBToddI8pDm48kPT1Nkx3ySqTQzCFwVjaPQEUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2doWNX6v4NUZXVvq55gptTczsEhgvyXzBMRDIk3x86TC9JvwGh1qtNWvMhYKnvaKhbA/image-asset.jpeg?format=2500w"},
+    {type: "image", name: "The Spice Channel", artist: "Wric White", year: "Unknown", dsc: "Description", link: "https://images.squarespace-cdn.com/content/v1/5065261fe4b0b0c3fa5adc93/1606445947263-AGJM7ZIM1RXJR1Z5WWBP/ke17ZwdGBToddI8pDm48kIPMjyAxYAbEcFTs8zCKKJJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0qN_-Z3B7EvygvPOPmeOryX1pkXg-pKyyA7MDP6ZuEHmJuzC89-qxFpXVqq6ylzIAA/***E+White+-The+Spice+Channel-+--NOT+FINAL+VERSION-PLEASE+PHOTO+in+AMS--+2020.jpg?format=2500w"},
+  ];
 
-const MenuItem = ({ text, selected }) => {
-  return <div className={`menu-item ${selected ? "active" : ""}`}><Zoom src={text}></Zoom></div>;
+const MenuItem = ({ type, name, artist, year, dsc, link, selected }) => {
+  if (type === "image") {
+    return <div className={`menu-item ${selected ? "active" : ""}`}>
+      <Zoom src={link}></Zoom>
+      <h3>{name} - {year}</h3>
+      <h4>{artist}</h4>
+      <h6>{dsc}</h6>
+    </div>;
+  } else if (type === "video/mp4" && link.includes("https://www.youtube.com/embed/")) {
+    return <div className={`menu-item ${selected ? "active" : ""}`}>
+    <iframe width="640" height="360" src={link} 
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>
+    <h1>{name}</h1>
+    <h2>{artist}</h2>
+    <h3>{year}</h3>
+    <h6>{dsc}</h6>
+  </div>;
+
+  } else {
+    return <div className={`menu-item ${selected ? "active" : ""}`}>
+      <video width="320" height="240" controls>
+        <source src={link} type={type} preload="auto"></source>
+        Your browser does not support the video tag.
+      </video>
+      <h1>{name}</h1>
+      <h2>{artist}</h2>
+      <h3>{year}</h3>
+      <h6>{dsc}</h6>
+    </div>;
+  }
+
 };
 
 export const Menu = (list, selected) =>
   list.map(el => {
-    const { name } = el;
-
-    return <MenuItem text={name} key={name} selected={selected} />;
+    // const { el } = el;
+    console.log("hiya");
+    console.log(el);
+    return <MenuItem key={el.link} type={el.type} name={el.name} artist={el.artist} year={el.year} dsc={el.dsc} link={el.link} selected={selected} />;
   });
 
 const Arrow = ({ text, className }) => {
